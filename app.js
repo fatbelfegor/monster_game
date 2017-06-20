617976722cc4b3bc94c5a42ecce136e8dc272c1a
Klim Agarkov <jewelgatorstv@gmail.com>
2017-06-21 00:55:55 +0300

    Ad log and finish the game

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameIsRunning: false
+    gameIsRunning: false,
+    turns: []
   },
   methods: {
     startGame: function() {
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function() {
-      this.monsterHealth -= this.calculateDamage(3,10);
+      var damage = this.calculateDamage(3,10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster for ' + damage
+      })
       if (this.checkWin()) {
           return;
         }
@@ -20,21 +27,39 @@ new Vue({
       this.monsterAttack();
     },
     specialAttack: function() {
-      this.monsterHealth -= this.calculateDamage(10,20);
+      var damage = this.calculateDamage(10,20);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster hard for ' + damage
+      })
       if (this.checkWin()) {
           return;
         }
       this.monsterAttack();
     },
     heal: function() {
-      this.playerHealth += 10;
+      if (this.playerHealth <= 90) {
+          this.playerHealth += 10;
+      } else {
+          this.playerHealth = 100;
+      }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heals himself for 10'
+      })
       this.monsterAttack();
     },
     giveUp: function() {
-
+      this.gameIsRunning = false;
     },
     monsterAttack: function() {
-      this.playerHealth -= this.calculateDamage(5,12);
+      var damage = this.calculateDamage(5,12);
+      this.playerHealth -= damage;
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hits Player for ' + damage
+      })
       this.checkWin();
     },
     calculateDamage: function(min, max) {
@@ -43,9 +68,9 @@ new Vue({
     checkWin: function() {
       if (this.monsterHealth <= 0) {
         if (confirm('You won! New Game?')) {
-          this.startGame();
+            this.startGame();
         } else {
-          this.gameIsRunning = false;
+            this.gameIsRunning = false;
         }
         return true;
       } else if (this.playerHealth <= 0) {
